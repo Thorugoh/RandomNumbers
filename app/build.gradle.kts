0b3edcdb81813cca21db6caacc3844898976bbbc
Victor Hugo <pavictorhugo@gmail.com>
2025-01-04 18:58:28 -0300

    add nested view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dev.thorugoh.randomnumbers"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dev.thorugoh.randomnumbers"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
